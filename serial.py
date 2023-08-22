b6b9993492f1e84704850c1a0fef58ad23623297
Gabe Yoro <gabrielyoro@gmail.com>
2023-08-22 16:02:34 -0700

    Add solution for Serial Generator

--- serial.py
+++ serial.py
@@ -17,3 +17,21 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self, start):
+        """Create serial with start and current initialized to start - 1"""
+        self.start = start - 1
+        self.current = start - 1
+    
+    def __repr__(self):
+        return f"""<Serial Generator, creates unique integer values 
+        beginning at start={self.start},
+        and tracking current={self.current}>"""
+
+    def generate(self):
+        """Increment current value by one"""
+        self.current += 1
+        return self.current
+
+    def reset(self):
+        """Resets current value to start value"""
+        self.current = self.start
